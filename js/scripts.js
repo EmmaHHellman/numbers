3cc2feae25b77d057a5e9951b6bb9d23757bc6c6
Emma Hellman <ehh801@icloud.com>
2020-06-12 12:05:18 -0700

    adds for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,37 @@
-// UI logic 
+
 $(document).ready(function() {
   $("form#enternumber").submit(function(event) {
-    const digitInput = $("input#number").val();
-
-    event.preventDefault();
+  
 
   // Business logic
 
   const beep = "beep",
   boop = "boop",
-  neighbor = "Won't you be my neighbor?";
+  beneighbor = "Won't you be my neighbor?";
 
   var numberArray = function(num) {
     const numberInput = [];
     for(i = 0; i <= num; i++) {
-      numberInput.push(i);
+      numberInput.push(i.toString());
     }
 
-  var message = numberInput.forEach(function(number) {
-    if (number.includes(3)){
-      return neighbor
-    } else if (number.includes(2))  {
+for (i = 0; i < numberInput.length; i++) {
+    if (numberInput[i].includes("3")){
+      return beneighbor
+    } else if (numberInput[i].includes("2"))  {
       return boop
-    } else if (number.includes(1)) {
+    } else if (numberInput[i].includes("1")) {
       return beep
     } else {
       return number
     }
-  });
+  };
 
   return message;
 }
+// UI logic 
+  const digitInput = $("input#number").val();
+  event.preventDefault();
   const covertedNumber = numberArray(digitInput);
   $("#output").text(covertedNumber)
 
