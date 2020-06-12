27fecef457bd52fbe0ffde45f09a599643f1ace0
Emma Hellman <ehh801@icloud.com>
2020-06-12 09:47:15 -0700

    adds business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,40 @@
+// UI logic 
 $(document).ready(function() {
-  $("form#numbers").submit(function(event) {
+  $("form#enternumber").submit(function(event) {
     const numberInput = $("input#number").val();
-    
-    $(".color").text(colorInput)
 
     event.preventDefault();
+
+  // Business logic
+  function convertNumber(numberInput) {
+
+    for (var i = 0; i < numberInput.length; i++) {
+      if(numberInput[i].includes(3)) {
+        console.log("three!")
+        if (numberInput[i].includes(2)) {
+          console.log("two!")
+          if (numberInput[i].includes(1)) {
+            console.log("one!")
+            return "Beep!";
+          }
+          return "Boop!";
+        }
+        return "Won't you be my neighbor?"
+      } else if (numberInput[i].includes(2)) {
+        console.log("two!")
+        if (numberInput[i].includes(1)) {
+          console.log("one!")
+          return "Beep!";
+        }
+        return "Boop!";
+      } else if (numberInput[i].includes(1)) {
+        return "Beep!";
+      }
+  }
+
+}
+  const covertedNumber = convertNumber(numberInput);
+  $("#output").text(covertedNumber)
+
   });
 });
