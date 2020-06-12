6d65dc9cb99a4f4c88220bc08ef969195a6d29ed
Emma Hellman <ehh801@icloud.com>
2020-06-12 15:44:58 -0700

    adds styling changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,48 @@
-
 $(document).ready(function() {
   $("form#enternumber").submit(function(event) {
-  
-  // Business logic
-  const beep = "beep",
-  boop = "boop",
-  beneighbor = "Won't you be my neighbor?";
 
-  var numberArray = function(num) {
-    const numberInput = [];
-    for (i = 0; i <= num; i++) {
-      numberInput.push(i.toString());
-    }
-
-    const numberOutput = [];
-
-    numberInput.forEach(function(number) { 
-      if (number.includes("3")){
-        numberOutput.push(beneighbor)
-        displayArray(numberOutput)
-      } else if (number.includes("2"))  {
-        numberOutput.push(boop)
-        displayArray(numberOutput)
-        $("#arrayoutput").text(numberOutput)
-      } else if (number.includes("1")) {
-        numberOutput.push(beep)
-        displayArray(numberOutput)
-        $("#arrayoutput").text(numberOutput)
-      } else {
-        numberOutput.push(number)
-        displayArray(numberOutput)
+    // Business logic
+    var numberArray = function(num) {
+      const numberInput = [];
+      for (i = 0; i <= num; i++) {
+        numberInput.push(i.toString());
       }
-    });
-  
-}
-// UI logic 
-  const digitInput = $("input#number").val();
-  event.preventDefault();
-  const covertedNumber = numberArray(digitInput);
- 
 
+      const beep = "beep",
+      boop = "boop",
+      beneighbor = "Won't you be my neighbor?",
+      numberOutput = [];
+
+      numberInput.forEach(function(number) {
+        if (number.includes("3")){
+          numberOutput.push(beneighbor)
+          displayArray(numberOutput)
+        } else if (number.includes("2"))  {
+          numberOutput.push(boop)
+          displayArray(numberOutput)
+          $("#arrayoutput").text(numberOutput)
+        } else if (number.includes("1")) {
+          numberOutput.push(beep)
+          displayArray(numberOutput)
+          $("#arrayoutput").text(numberOutput)
+        } else {
+          numberOutput.push(number)
+          displayArray(numberOutput)
+        }
+      });
+    }
+    
+  // UI logic 
+    const digitInput = $("input#number").val();
+    numberArray(digitInput);
+  
     function displayArray(num) {
       const dividedNum = num;
       $("#arrayoutput").text(dividedNum);
     }
- 
+
+    event.preventDefault();
+  
   });
 });
 
