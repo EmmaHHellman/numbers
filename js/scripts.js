f533cb911fdc258e53b811ab5d304eeaa5683f37
Emma Hellman <ehh801@icloud.com>
2020-06-12 11:26:54 -0700

    adds dry code to business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,39 @@
 // UI logic 
 $(document).ready(function() {
   $("form#enternumber").submit(function(event) {
-    const numberInput = $("input#number").val();
+    const digitInput = $("input#number").val();
 
     event.preventDefault();
 
   // Business logic
-  function convertNumber(numberInput) {
 
-    var numberoutput = [];
-    for (var i = 0; i < numberInput.length; i++) {
-      if(numberInput[i].includes(3)) {
-        console.log(numberInput)
-        if (numberInput[i].includes(2)) {
-          console.log("two!")
-          if (numberInput[i].includes(1)) {
-            console.log("one!")
-            return "Beep!";
-          }
-          return "Boop!";
-        }
-        return "Won't you be my neighbor?"
-      } else if (numberInput[i].includes(2)) {
-        console.log("two!")
-        if (numberInput[i].includes(1)) {
-          console.log("one!")
-          return "Beep!";
-        }
-        return "Boop!";
-      } else if (numberInput[i].includes(1)) {
-        return "Beep!";
-      }
-      const arrayOutput = numberoutput.push(i);
-      console.log(arrayOutput);
-  }
+  const beep = "beep",
+  boop = "boop",
+  neighbor = "Won't you be my neighbor?";
 
+  var numberArray = function(num) {
+    const numberInput = [];
+    for(i = 0; i <= num; i++) {
+      numberInput.push(i);
+    }
+
+  var message = numberInput.forEach(function(number) {
+    if (number.includes(3)){
+      return neighbor
+    } else if (number.includes(2))  {
+      return boop
+    } else if (number.includes(1)) {
+      return beep
+    } else {
+      return number
+    }
+  });
+
+  return message;
 }
-  const covertedNumber = convertNumber(numberInput);
+  const covertedNumber = numberArray(digitInput);
   $("#output").text(covertedNumber)
 
   });
 });
+
